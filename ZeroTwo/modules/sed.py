74541e5091b652abd76bd3abcfad8116fd85af68
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-05-27 17:48:49 +0530

    Update sed.py

--- ZeroTwo/modules/sed.py
+++ ZeroTwo/modules/sed.py
@@ -115,21 +115,9 @@ def sed(update: Update, context: CallbackContext):
         elif text:
             update.effective_message.reply_to_message.reply_text(text)
 
-__mod_name__ = "Sed/Regex"            
-
-__help__ = """
- • `s/<text1>/<text2>(/<flag>)`*:* Reply to a message with this to perform a sed operation on that message, replacing all \
-occurrences of 'text1' with 'text2'. Flags are optional, and currently include 'i' for ignore case, 'g' for global, \
-or nothing. Delimiters include `/`, `_`, `|`, and `:`. Text grouping is supported. The resulting message cannot be \
-larger than {}.
-*Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
-If you want to use these characters, make sure you escape them!
-*Example:* \\?.
-""".format(telegram.MAX_MESSAGE_LENGTH)
 
 SED_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r's([{}]).*?\1.*'.format("".join(DELIMITERS))),
     sed,
     friendly="sed")
-
 dispatcher.add_handler(SED_HANDLER)
