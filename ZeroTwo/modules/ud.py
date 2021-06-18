566a82a041c288732c11303b26dffd7f2013c372
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-06-18 22:57:29 +0530

    Update ud.py

--- ZeroTwo/modules/ud.py
+++ ZeroTwo/modules/ud.py
@@ -16,11 +16,36 @@ def ud(update: Update, context: CallbackContext):
     except:
         reply_text = "No results found."
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    
+    
+    
+#Dictionary module by @TheRealPhoenixBot.
+@run_async
+def define(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    word = msg.text[len('/def '):]
+    res = requests.get(f"https://googledictionaryapi.eu-gb.mybluemix.net/?define={word}")
+    if res.status_code == 200:
+        info = res.json()[0].get("meaning")
+        if info:
+            meaning = ""
+            for count, (key, value) in enumerate(info.items(), start=1):
+                meaning += f"<b>{count}. {word}</b> <i>({key})</i>\n"
+                for i in value:
+                    defs = i.get("definition")
+                    meaning += f"• <i>{defs}</i>\n"
+            message.reply_text(meaning, parse_mode=ParseMode.HTML)
+        else:
+            return 
+    else:
+        message.reply_text("No results found!")
 
 
 UD_HANDLER = DisableAbleCommandHandler(["ud"], ud)
+DEFINE_HANDLER = DisableAbleCommandHandler(["dict", "def"], define)
 
 dispatcher.add_handler(UD_HANDLER)
+dispatcher.add_handler(DEFINE_HANDLER)
 
-__command_list__ = ["ud"]
-__handlers__ = [UD_HANDLER]
+__command_list__ = ["ud", "def"]
+__handlers__ = [UD_HANDLER, DEFINE_HANDLER]
