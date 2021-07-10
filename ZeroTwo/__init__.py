d1b474dfd7a079207ac4a54a6dfc112726d8122a
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-07-10 14:21:17 +0530

    Update __init__.py

--- ZeroTwo/__init__.py
+++ ZeroTwo/__init__.py
@@ -90,6 +90,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
+    DICT_API = os.environ.get('DICT_API', None)
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
