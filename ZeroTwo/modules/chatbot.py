5a6f5ef67f5e7c3f7d0a12224881d007ab54bfad
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-07-15 12:23:44 +0530

    Update chatbot.py

--- ZeroTwo/modules/chatbot.py
+++ ZeroTwo/modules/chatbot.py
@@ -3,6 +3,16 @@ import re
 from aiohttp import ClientSession
 
 from ZeroTwo import ARQ_API
+from ZeroTwo import telethn as ZeroTwo
 
 from Python_ARQ import ARQ
 
+aiohttp_session = ClientSession()
+arq = ARQ(ARQ_URL, ARQ_API, aiohttp_session)
+
+async def getresp(query: str, user_id: int):
+  bot = await arq.bot(query, user_id)
+  response = bot.result
+  return response
+
+@ZeroTwo.on_message(filters.command(
